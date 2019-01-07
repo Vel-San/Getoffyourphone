c5b1f3d20a0dd3511d2bd995a9ff432f172aced2
Ali <ali-k95@hotmail.com>
2019-01-07 14:39:47 +0100

    Reboot Detection
    - Added Timer Service fix on phone reboot

---
+++ app/src/main/java/com/nephi/getoffyourphone/BootUpReceiver.java
@@ -0,0 +1,31 @@
+package com.nephi.getoffyourphone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class BootUpReceiver extends BroadcastReceiver {
+
+    //Database
+    DB_Helper db;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //DataBase Handler
+        db = new DB_Helper(context);
+
+        if(Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED))
+        {
+            db.set_Running("N");
+        }
+
+        Toast.makeText(context, "Phone Has Been Rebooted - Timer Service Fixed", Toast.LENGTH_LONG).show();
+        Log.d("PhoneReboot", "Phone Has Been Rebooted");
+    }
+
+}
